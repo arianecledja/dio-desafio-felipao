8eb02f02e3e620c7f6602e446d31da3535b7cd18
Ariane Santos <ariane225040@gmail.com>
2024-01-16 10:54:14 -0300

    added conditional structure - switch

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-const name = 'Paula'
-const xp = 1005
+const name = 'Bulbasaur'
+const xp = 7505
 let level = ''
+let key = true
 
-if(xp < 1000) {
-    level = 'Ferro'
-} else if(xp > 1001 && xp < 2000) {
-    level = 'Bronze'
-} else if(xp > 2001 && xp < 5000) {
-    level = 'Prata'
-} else if(xp > 5001 && xp < 7000) {
-    level = 'Ouro'
-} else if(xp > 7001 && xp < 8000) {
-    level = 'Platina'
-} else if(xp > 8001 && xp < 9000) {
-    level = 'Ascendente'
-} else if(xp > 9001 && xp < 10000) {
-    level = 'Imortal'
-} else if(xp > 10001) {
-    level = 'Radiante'
-} else {
-    level = 'Valor inválido'
+switch (key) {
+    case (xp < 1001):
+        level = 'Ferro'
+        break;
+    case (xp > 1001 && xp < 2000):
+        level = 'Bronze'
+        break;
+    case (xp > 2001 && xp < 5000):
+        level = 'Prata' 
+        break
+    case (xp > 5001 && xp < 7000):
+        level = 'Ouro'
+        break
+    case (xp > 7001 && xp < 8000):
+        level = 'Platina'
+        break
+    case (xp > 8001 && xp < 9000):
+        level = 'Ascendente'
+        break
+    case (xp > 9001 && xp < 10000):
+        level = 'Imortal'
+        break
+    case (xp > 10001):
+        level = 'Radiante'
+        break
+    default:
+        level = 'Valor inválido'
+        break;
 }
 
 console.log(`O Herói de nome ${name} está no nível de ${level}`)
